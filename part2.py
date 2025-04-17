47c006c97feb8e82fbc588051f651e111ca85660
poornima1508 <45503926+poornima1508@users.noreply.github.com>
2025-04-16 20:21:06 -0500

    Update part2.py

--- part2.py
+++ part2.py
@@ -5,6 +5,18 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Load dataset with column names
+
+column_names = [
+    "symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", 
+    "body-style", "drive-wheels", "engine-location", "wheel-base", "length", "width", 
+    "height", "curb-weight", "engine-type", "num-of-cylinders", "engine-size", "fuel-system", 
+    "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg", "highway-mpg", 
+    "price"
+]
+df = pd.read_csv('CleanedAutomobile.csv', header=None, names=column_names)
+
+
 #Question 1: What is the data type of the column "peak-rpm"?
 
 print(df['peak-rpm'].dtype)
